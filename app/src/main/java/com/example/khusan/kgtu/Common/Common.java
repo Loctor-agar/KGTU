1425efb979dbd0a4a3b76bf69a44b7b98739cba4
Loctor-agar <khusan.mukhammetrakhimov@gmail.com>
2018-07-05 11:47:52 +0300

    autentification

---
+++ app/src/main/java/com/example/khusan/kgtu/Common/Common.java
@@ -0,0 +1,7 @@
+package com.example.khusan.kgtu.Common;
+
+import com.example.khusan.kgtu.Model.User;
+
+public class Common {
+    public static User currentUser;
+}
---
+++ app/src/main/java/com/example/khusan/kgtu/Model/User.java
@@ -0,0 +1,32 @@
+package com.example.khusan.kgtu.Model;
+
+public class User {
+    private String Name;
+    private String Password;
+
+
+    public User() {
+    }
+
+    public User(String name,String password){
+        Name = name;
+        Password = password;
+
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+}
